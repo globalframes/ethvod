0361e7ef3f11d473ce69bb0a82601a0511d6345a
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2022-04-23 01:56:06 +0200

    wip: check signature

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const restify = require("restify");
 const corsMiddleware = require("restify-cors-middleware2");
 const jsonfile = require('jsonfile')
+const Accounts = require('web3-eth-accounts');
+const accounts = new Accounts();
 
 console.log("Starting server...");
 
@@ -16,6 +18,8 @@ const cors = corsMiddleware({
     ]
 });
 
+const CHALLENGE = "f6129a2dc6c41ef6d6af"
+
 server.pre(cors.preflight);
 server.use(cors.actual);
 server.use(restify.plugins.bodyParser());
@@ -25,6 +29,20 @@ server.get("/test", (req, res) => {
     res.send(200, "Test OK");
 });
 
+server.get("/challenge", (req, res) => {
+    res.send(200, `${CHALLENGE}`);
+});
+
+
+server.get("/r", (req, res) => {
+    res.send(200, `${CHALLENGE}`);
+});
+
+
 server.listen(9999, function () {
     console.log("%s listening at %s", server.name, server.url);
+    const challenge = "Hello world";
+    const signature = "0x30755ed65396facf86c53e6217c52b4daebe72aa4941d89635409de4c9c7f9466d4e9aaec7977f05e923889b33c0d0dd27d7226b6e6f56ce737465c5cfd04be400"
+    const recoveredAddress = accounts.recover(challenge, signature)
+    console.log(recoveredAddress)
 });
