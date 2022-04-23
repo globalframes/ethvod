effeb76b439fba6fed8a40e642b45a3790945de1
geleeroyale <krassvs@gmail.com>
2022-04-23 20:01:30 +0200

    styling

--- frontend/src/pages/upload/components/Stream.js
+++ frontend/src/pages/upload/components/Stream.js
@@ -1,76 +1,84 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 // import "./css/style.sass";
-import axios from "axios";
-import { useForm } from "react-hook-form";
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 import { StyledDropZone } from 'react-drop-zone'
 import 'react-drop-zone/dist/styles.css'
 
-function Comp({ onLoggedin }) {
+function Comp ({ onLoggedin }) {
+  const [ready, setReady] = useState(false)
+  // On file select (from the pop up)
+  const [file, setFile] = useState()
+  const [title, setTitle] = useState()
+  const [description, setDescription] = useState()
 
-    const [ready, setReady] = useState(false);
-    // On file select (from the pop up)
-    const [file, setFile] = useState();
-    const [title, setTitle] = useState();
-    const [description, setDescription] = useState();
+  const createStream = () => {
+    if (!file) return
 
-    const createStream = () => {
-        if (!file) return;
+    // // Create an object of formData
+    const formData = new FormData()
+    formData.append('title', title)
+    formData.append('description', description)
 
+    // // Update the formData object
+    // formData.append(
+    //     "videoFile",
+    //     file,
+    //     file.name
+    // );
 
-        // // Create an object of formData
-        const formData = new FormData();
-        formData.append('title', title);
-        formData.append('description', description);
+    // // Details of the uploaded file
+    // console.log(file);
 
-        // // Update the formData object
-        // formData.append(
-        //     "videoFile",
-        //     file,
-        //     file.name
-        // );
+    // Request made to the backend api
+    // Send formData object
+    // axios.post("http://localhost:9999/stream", formData);
+  }
 
-        // // Details of the uploaded file
-        // console.log(file);
+  return (
+    <>
+      <h2 className='subtitle  has-text-white is-4'>Start streaming</h2>
+      <div>
+        <div className='field'>
+          <label className='label'>Stream Title</label>
+          <div className='control'>
+            <input
+              className='input'
+              type='text'
+              placeholder='Stream title'
+              name='Title'
+              onChange={e => {
+                setTitle(e.target.value)
+              }}
+            />
+          </div>
+        </div>
+        <div className='field'>
+          <label className='label'>Description</label>
+          <div className='control'>
+            <textarea
+              className='textarea'
+              placeholder='Stream description'
+              onChange={e => {
+                setDescription(e.target.value)
+              }}
+            ></textarea>{' '}
+          </div>
+        </div>
 
-        // Request made to the backend api
-        // Send formData object
-        // axios.post("http://localhost:9999/stream", formData);
-
-    }
-
-    return (
-        <>
-            <h2 className="subtitle  has-text-white is-4">
-                Start streaming
-            </h2>
-            <div>
-                <div className="field">
-                    <label className="label">Stream Title</label>
-                    <div className="control">
-                        <input className="input" type="text" placeholder="Stream title" name="Title" onChange={(e) => { setTitle(e.target.value); }} />
-                    </div>
-                </div>
-                <div className="field">
-                    <label className="label">Description</label>
-                    <div className="control">
-                        <textarea className="textarea" placeholder="Stream description" onChange={(e) => { setDescription(e.target.value) }}></textarea>                    </div>
-                </div>
-
-
-                <div>
-                    <button className="button is-medium is-link" onClick={(e) => { createStream() }}>
-                        Start streaming!
-                    </button>
-                </div>
-
-            </div>
-
-
-
-
-        </>
-    );
-
+        <div>
+          <button
+            className='button is-medium'
+            onClick={e => {
+              createStream()
+            }}
+          >
+            Start streaming!
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Comp;
+export default Comp
